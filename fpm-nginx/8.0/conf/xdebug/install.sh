c32822b91521415280f104ea83a90d3b7033d055
Chrudos Vorlicek <chrudos.vorlicek@gmail.com>
2021-05-10 17:10:53 +0200

    Upgrade xdebug to 3.0.4

--- fpm-nginx/8.0/conf/xdebug/install.sh
+++ fpm-nginx/8.0/conf/xdebug/install.sh
@@ -7,5 +7,5 @@ echo "Copying xdebug initial scripts to the global init directory"
 cp -R "${current_dir}/init.d/." "${container_init_directory}"
 
 echo "Installing xdebug extension"
-    pecl install xdebug-3.0.2 \
+    pecl install xdebug-3.0.4 \
     && docker-php-ext-enable xdebug
